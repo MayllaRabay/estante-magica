b83a474cb0119160ffb925840d5bc03caaea8ad1
Maylla Rabay <mayllarabay@gmail.com>
2022-01-09 01:15:15 -0300

    Pacote react-hook-form instalado; Tratamento e estilização do formulário iniciados

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,15 +1,85 @@
 import { useState } from 'react';
+import { useForm } from 'react-hook-form';
+import { useNavigate } from 'react-router-dom';
+import styles from './styles.module.scss';
 
-function Form() {
+function Form({ role }) {
+  const { formState: { errors }, handleSubmit, register } = useForm();
+  const navigate = useNavigate();
   const [checked, setChecked] = useState(false);
-  
+
+  function onSubmit(data) {
+    console.log(data);
+  }
+
+  function backButton() {
+    navigate("/");
+  }
+
   return(
-    <>
-      <label>
-        <input id="checkbox" type="checkbox" checked={checked} onChange={() => setChecked(!checked)} />
-        Li e aceito os <span>Termos de Uso</span> e a <span>Política de Privacidade</span>.
-      </label>
-    </>
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <div className={styles.form__field}>
+        <label htmlFor="name">Nome</label>
+        <input id="name" type="text"
+          {...register("name", { required: true, minLength: 6 })}
+        />
+        {errors.name && <div className={styles.error__message}>Campo obrigatório</div>}
+      </div>
+
+      <div className={styles.form__field}>
+        <label htmlFor="phone">Celular</label>
+        <input id="phone" type="text"
+          {...register("phone", { required: true, minLength: 11, maxLength: 11 })}
+        />
+        {errors.phone && <div className={styles.error__message}>Campo obrigatório</div>}
+      </div>
+
+      {(role === "teacher" || role === "manager") &&
+        <div className={styles.form__field}>
+          <label htmlFor="school">Tipo da escola</label>
+          <input id="school" type="dropdown"
+            {...register("school", { required: true })}
+          />
+          {errors.school && <div className={styles.error__message}>Campo obrigatório</div>} 
+        </div>
+      }
+
+      <div className={styles.form__field}>
+        <label htmlFor="email">E-mail</label>
+        <input id="email" type="text"
+          {...register("email", { required: true })} 
+        />
+        {errors.email && <div className={styles.error__message}>Campo obrigatório</div>}
+      </div>
+
+      {(role === "teacher" || role === "manager") &&
+        <div className={styles.form__field}>
+          <label htmlFor="password">Senha</label>
+          <input id="password" type="text"
+            {...register("password", { required: true, minLength: 6 })} 
+          />
+          <div className={errors.password ? styles.error__message : styles.field__message}>Sua senha deve conter pelo menos 6 dígitos</div>
+          {errors.password?.type === "required" && <div className={styles.error__message}>Campo obrigatório</div>}
+        </div>
+      }
+      
+      <div className={styles.form__field}>
+        <div className={styles.terms__field}>
+          <label htmlFor="terms">
+            Li e aceito os <span>Termos de Uso</span> e a <span>Política de Privacidade</span>.
+          </label>
+          <input id="terms" type="checkbox" checked={checked} onChange={() => setChecked(!checked)}
+            {...register("terms", { required: true })}
+          />
+        </div>
+        {errors.terms && <div className={styles.error__message}>Campo obrigatório</div>}
+      </div>
+      
+      <div className={styles.button__wrapper}>
+        <button type="submit" className={styles.main__button}>Cadastrar</button>
+        <button onClick={backButton} className={styles.secondary__button}>Voltar</button>
+      </div>
+    </form>
   );
 }
 
--- src/components/FormContainer/index.js
+++ src/components/FormContainer/index.js
@@ -1,30 +1,18 @@
-import { useNavigate } from 'react-router-dom';
+import Form from '../Form';
 import styles from './styles.module.scss';
 
-function FormContainer({ img, title, role, form }) {
-  const navigate = useNavigate();
-
-  function backButton() {
-    navigate("/");
-  }
-
+function FormContainer({ img, title, role }) {
   return(
     <div className={styles.content__wrapper}>
       <div className={styles.img__container}>
         <img src={img} alt="" />
       </div>
       <h1>{title}</h1>
-      {role === "teacher" || role === "manager"
+      {(role === "teacher" || role === "manager")
         ? <h4>Se você já tiver uma conta, <span>faça seu login</span>.</h4>
         : <p>Só é possível aplicar o projeto por uma escola. Mas você pode acompanhar tudo de perto, recebendo nossos conteúdos!</p>
       }
-      <form>
-        {form}
-        <div className={styles.button__wrapper}>
-          <button type="submit" className={styles.main__button}>Cadastrar</button>
-          <button onClick={backButton} className={styles.secondary__button}>Voltar</button>
-        </div>
-      </form>
+      <Form role={role}/>
     </div>
   );
 }
